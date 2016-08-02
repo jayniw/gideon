226eb6d6ebb117c89c1af2e57b9bf097bbae6054
jayniw <jayniw@gmail.com>
2016-08-02 09:45:16 -0400

    primera version

---
+++ app/pages/home/home.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {PaginaPage} from '../pagina/pagina'
+
+@Component({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor(private navCtrl: NavController) {
+  
+  }
+
+  goToPaginaPage(){
+    this.navCtrl.push( PaginaPage );
+  }
+}
---
+++ app/pages/pagina/pagina.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the PaginaPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/pagina/pagina.html',
+})
+export class PaginaPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
