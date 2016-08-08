c2a4fe4a5c635ec0cd74db36f01cdf90a8dd0b49
jayniw <jayniw@gmail.com>
2016-08-08 19:06:17 -0400

    probando conexion con firebase

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {TabsPage} from './pages/tabs/tabs';
-
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
 @Component({
   template: '<ion-nav [root]="rootPage"></ion-nav>'
@@ -22,4 +22,12 @@ export class MyApp {
   }
 }
 
-ionicBootstrap(MyApp);
+ionicBootstrap(MyApp,[
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: "AIzaSyAfCmIKd4oQlZwd3-T5DVpsAeZPKExbj0s",
+    authDomain: "project-6335036855707023643.firebaseapp.com",
+    databaseURL: "https://project-6335036855707023643.firebaseio.com",
+    storageBucket: "project-6335036855707023643.appspot.com",
+  }),
+]);
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,13 +1,57 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {PaginaPage} from '../pagina/pagina';
+import {FirebaseListObservable, FirebaseDatabase} from 'angularfire2';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
+
+  tasks: FirebaseListObservable<any>;
+
+  constructor(private navCtrl: NavController, 
+              private database: FirebaseDatabase
+             ) {
+      this.tasks = this.database.list('/tasks')
+  }
+
+  createTask(){
+    let newTaskModal = Alert.create({
+      title: 'Nueva Tarea',
+      message: "Ingrese un titulo para tu nueva tarea",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Titulo'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            this.tasks.push({
+              title: data.title,
+              done: false
+            });
+          }
+        }
+      ]
+    });
+    this.navCtrl.present( newTaskModal );
+  }
+
+  updateTask( task ){
+    setTimeout(()=>{
+      this.tasks.update(
+         task.$key,{
+           title: task.title,
+           done: task.done
+         }
+      )
+    }
+
+    )
   }
 
   goToPaginaPage(){
