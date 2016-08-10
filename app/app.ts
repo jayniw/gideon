480cb91ea424db0a1fa8e485e24d19a08a577059
jayniw <jayniw@gmail.com>
2016-08-10 17:14:20 -0400

    quitar tabs y generar sidemenu

--- app/app.ts
+++ app/app.ts
@@ -1,26 +1,47 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
+import {Component, ViewChild } from '@angular/core';
+import {Platform, ionicBootstrap, Nav} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-//import {TabsPage} from './pages/tabs/tabs';
-import {HomePage} from './pages/home/home';
+/*Para manejo y conexion a firebase*/
 import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
+/*Importar las paginas del proyecto*/
+import {HomePage} from './pages/home/home';
+import {MisionesPage} from './pages/misiones/misiones';
+import {GestasPage} from './pages/gestas/gestas';
+import {GuerrasPage} from './pages/guerras/guerras';
 
 @Component({
-  template: '<ion-nav #content [root]="rootPage"></ion-nav>'
+  //template: '<ion-nav #content [root]="rootPage"></ion-nav>'
+  templateUrl: 'build/app.html'
 })
 export class MyApp {
-
-  private rootPage: any;
+  @ViewChild(Nav) nav: Nav;
+  rootPage: any = HomePage;
+  pages: Array<{title: string, component: any}>;
 
   constructor(private platform: Platform, af: AngularFire) {
-    this.rootPage = HomePage;
+    this.initializeApp();
+    this.pages = [
+      { title: 'Inicio', component: HomePage },
+      { title: 'Misiones', component: MisionesPage },
+      { title: 'Gestas', component: GestasPage },
+      { title: 'Guerras', component: GuerrasPage }
+    ];
+  }
 
-    platform.ready().then(() => {
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
   }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+  
 }
 
 ionicBootstrap(MyApp,[
---
+++ app/pages/misiones/misiones.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the MisionesPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/misiones/misiones.html',
+})
+export class MisionesPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
